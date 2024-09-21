e84d6beee8d5da823e329f474c24ace843a2b9f3
wins0med33r <patrik.atkinson1@gmail.com>
2024-09-21 18:43:26 +0100

    FIX - Tests are running here

--- Tests/Matrix Tests/MatrixTests.cpp
+++ Tests/Matrix Tests/MatrixTests.cpp
@@ -1,14 +1,15 @@
 #define BOOST_TEST_MODULE MatrixTests
 
 #include <boost/test/included/unit_test.hpp>
-#include "Matrix/Matrix.h"
-#include "Matrix/Append.h"
+#include <Matrix.hpp> 
+#include <Matrix.cpp> 
 
-BOOST_AUTO_TEST_CASE(Append_AddElement)
+BOOST_AUTO_TEST_CASE(ScalarMultiplication_Operator_Test)
 {
 	mp::Matrix<int> test(2, 2);
 
-	mp::append<int>(test, 1);
+	test[0][1] = 1;
+	test *= 10;
 
-	BOOST_ASSERT(test[3][0] == 1);
+	BOOST_ASSERT(test[0][1] == 10);
 }
--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,19 +1,21 @@
-﻿#pragma once
-#include "Append.h"
+﻿#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <cstddef>
+#include <stdexcept>
+#include <algorithm>
+#include <type_traits>
 
 namespace mp
 {
 	template<typename T>
 	class Matrix
 	{
-	private:
+	public:
 
 		T* _data = nullptr;
 		size_t _row = 0, _col = 0;
-
 		void resize();
-
-	public:
 
 		// Constructors
 		Matrix(size_t m, size_t n);
@@ -31,32 +33,23 @@ namespace mp
 		Matrix<T>(Matrix<T>&& other) noexcept;
 
 		// Move Assignment
-		Matrix<T>& operator=(Matrix<T>&& other) noexcept;
+		Matrix<T>& operator=(const Matrix<T>&& other) noexcept;
 
 		/* Overloaded Operators */
 
-		Matrix<T>& operator+=(Matrix<T>& other);
+		Matrix<T>& operator+=(const Matrix<T>& other);
 
-		Matrix<T>& operator-=(Matrix<T>& other);
+		Matrix<T>& operator-=(const Matrix<T>& other);
 
-		Matrix<T>& operator*=(Matrix<T>& other);
+		Matrix<T>& operator*=(const Matrix<T>& other);
 
 		Matrix<T>& operator*=(T scalar);
 
 		T* operator[](size_t row);
 
 		// Append Method
-		template<typename T>
-		friend void append<T>(Matrix<T>& mat, T input);
+		void append(T input);
 	};
-
-	template<typename T>
-	Matrix<T>::Matrix(size_t m, size_t n) : _row(m), _col(n), _data(new T[m * n]()) {}
-
-	template<typename T>
-	Matrix<T>::~Matrix()
-	{
-		delete[] _data;
-		_data = nullptr;
-	}
 }
+
+#endif // MATRIX_H

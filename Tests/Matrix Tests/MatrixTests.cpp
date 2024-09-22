89027c84c14994f2552dbb35ce7a2af98f856218
wins0med33r <patrik.atkinson1@gmail.com>
2024-09-22 21:40:07 +0100

    Change the CmakeLists.txt file and changed from boost to gtest

--- Tests/Matrix Tests/MatrixTests.cpp
+++ Tests/Matrix Tests/MatrixTests.cpp
@@ -1,15 +1,13 @@
-#define BOOST_TEST_MODULE MatrixTests
-
-#include <boost/test/included/unit_test.hpp>
+#include <gtest/gtest.h>
 #include <Matrix.hpp> 
 #include <Matrix.cpp> 
 
-BOOST_AUTO_TEST_CASE(ScalarMultiplication_Operator_Test)
+TEST(scalar_multiplication_operator_tests, ScalarMultiplication_Operator_Test)
 {
 	mp::Matrix<int> test(2, 2);
 
 	test[0][1] = 1;
 	test *= 10;
-
-	BOOST_ASSERT(test[0][1] == 10);
+	
+	EXPECT_EQ(test[0][1], 10);
 }
